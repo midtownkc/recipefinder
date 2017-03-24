19bccd2be771b27f4ed2142cb0ac0a9510308f60
Maxwell Frederickson <maxwellfrederickson@gmail.com>
2017-03-24 10:21:51 -0500

    Finally got API working

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,17 +1,19 @@
+#require 'httparty'
 class Recipe
   include HTTParty
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com/api'
-  #base_uri "http://#{hostport}"
-  base_uri = 'http://food2fork.com:80/api'
+  base_uri "http://#{hostport}"
+  #base_uri 'http://food2fork.com/api'
   default_params key: ENV["FOOD2FORK_KEY"]
   format :json
 
-  def self.for(ingredient = "chocolate")
-    # issues an HTTP GET request using the HTTParty gem
-    # the HTTP GET request must have the "q=keyword" query argument and query "/search" route
-    # returns the JSON payload document supplied in the recipes element of the hash returned by HTTParty
-    recipes_returned = get("/search", query: {q: ingredient})["recipes"]
-    #Array(chocolates)
+  def self.for (ingredient)
+    begin
+      get("/search", query: {q: ingredient})["recipes"]
+    rescue Exception => e
+      e
+    end
   end
-
 end
+#testy = Recipe.for("cheese")
+#puts testy
