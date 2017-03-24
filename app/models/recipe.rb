bb520666589b76e0bb434f17d517d6ddd5fc7c77
Maxwell Frederickson <maxwellfrederickson@gmail.com>
2017-03-24 12:59:53 -0500

    passed all tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,6 @@ class Recipe
   include HTTParty
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com/api'
   base_uri "http://#{hostport}"
-  #base_uri 'http://food2fork.com/api'
   default_params key: ENV["FOOD2FORK_KEY"]
   format :json
 
@@ -15,5 +14,3 @@ class Recipe
     end
   end
 end
-#testy = Recipe.for("cheese")
-#puts testy
